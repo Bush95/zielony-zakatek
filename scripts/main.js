6a2fe5d6bd39681ff885cce0b551ab98146ba1fc
Bush95 <adi139597@gmail.com>
2016-08-20 00:29:14 +0200

    mobile finished, added tablet and desktop to test. Slider only to go

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
 (function () {
     $(document).ready(function () {
-        var toggle_menu = function () {
-            $('.page-nav').slideToggle(200);
-            $('#hamburger').toggleClass('nav-open');
-        };
-        $('#hamburger').on('click', toggle_menu);
+        var isMobile = true;
+        $('#hamburger').css('display') != 'none' ? isMobile = true : isMobile = false;
+        if (isMobile) {
+            var toggle_menu = function () {
+                $('.page-nav').slideToggle(200);
+                $('#hamburger').toggleClass('nav-open');
+            };
+            $('#hamburger').on('click', toggle_menu);
+        }
         $('.page-nav a').on('click', function (e) {
             e.preventDefault();
-            toggle_menu();
+            isMobile ? toggle_menu() : '';
             var dest = $($(this).attr('href').replace('#', '.')).offset().top;
             $('html, body').animate({
                 scrollTop: dest
