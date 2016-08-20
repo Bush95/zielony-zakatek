bd37f9273855ec53de3f1b142b2a8f198c578d7e
Bush95 <adi139597@gmail.com>
2016-08-20 12:44:52 +0200

    Slide basic script, need improvments, fixed outline

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
     $(document).ready(function () {
         var isMobile = true;
         $('#hamburger').css('display') != 'none' ? isMobile = true : isMobile = false;
+        
         if (isMobile) {
             var toggle_menu = function () {
                 $('.page-nav').slideToggle(200);
@@ -9,6 +10,7 @@
             };
             $('#hamburger').on('click', toggle_menu);
         }
+        
         $('.page-nav a').on('click', function (e) {
             e.preventDefault();
             isMobile ? toggle_menu() : '';
@@ -17,5 +19,37 @@
                 scrollTop: dest
             }, 800);
         });
+        
+        //Slider
+        
+        if (!isMobile) {
+            var slides_num = $('#slider div').length;
+            var currentSlideNum = 0;
+            var nextSlideNum = 0;
+            var animationTime = 400;
+            setInterval( function () {
+                var currentSlide = $('#slider div')[currentSlideNum];
+                if (currentSlideNum == slides_num-1) {
+                    nextSlideNum = 0;
+                } else {
+                    nextSlideNum++;
+                }
+                var nextSlide = $('#slider div')[nextSlideNum];
+                
+                $(nextSlide).animate({
+                    'opacity' : 1
+                }, animationTime);
+                
+                $(currentSlide).animate({
+                    opacity: 0
+                }, animationTime);
+                
+                console.log(currentSlideNum);
+                console.log(nextSlideNum);
+                console.log(nextSlide);
+                currentSlideNum = nextSlideNum;
+                
+            }, 3000);
+        }
     });
 })();
